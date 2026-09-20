a67364cdc1043c03f0bd85400fc666301d4ea7d4
idotick <sanderdalangin@gmail.com>
2026-09-20 08:39:38 +0800

    polished jump, dash, collisions

--- gameplay/player.gd
+++ gameplay/player.gd
@@ -19,19 +19,27 @@ extends CharacterBody2D
 @export var decel_duration: float = 0.5
 
 @export var max_speed: float = 300.0
+@export var dash_speed: float = 500.0
+@export var dash_duration: float = 0.3
 
 @onready var jump_frame = Time.get_unix_time_from_system()
 @onready var fall_frame = Time.get_unix_time_from_system()
 @onready var accel_frame = Time.get_unix_time_from_system()
 @onready var decel_frame = Time.get_unix_time_from_system()
+@onready var dash_frame = Time.get_unix_time_from_system()
 
 @onready var was_on_floor: bool = false
+@onready var jump_buffered: bool = false
+@onready var on_coyote: bool = false
+@onready var can_dash: bool = true
 
 @onready var state_manager: StateManager
 @onready var curr_states: Array[StateMachine.State] = []
 @onready var state_machine : StateMachine = StateMachine.new()
 
 @onready var move_speed: float = 0.0
+@onready var move_dash: float = 0.0
+@onready var prev_direction: int = 1
 
 func _ready() -> void:
 	add_state(StateMachine.State.IDLE)
@@ -59,8 +67,12 @@ func jump():
 		return
 	if (!curr_states.has(StateMachine.State.FALLING)):
 		return
+	
 	add_state(StateMachine.State.JUMPING)
 	curr_states.erase(StateMachine.State.FALLING)
+	get_tree().call_group("JumpTimers", "stop")
+	jump_buffered = false
+	on_coyote = false
 	
 	jump_frame = Time.get_unix_time_from_system()
 	
@@ -81,13 +93,17 @@ func decel():
 	add_state(StateMachine.State.IDLE)
 	curr_states.erase(StateMachine.State.MOVING)
 	decel_frame = Time.get_unix_time_from_system()
+
+func dash():
+	if (curr_states.has(StateMachine.State.DASHING)):
+		return
 	
+	add_state(StateMachine.State.DASHING)
+	dash_frame = Time.get_unix_time_from_system()
+	can_dash = false
 
 func _physics_process(delta: float) -> void:
 	var curr_frame: float = Time.get_unix_time_from_system()
-	
-	if not is_on_floor() and (not curr_states.has(StateMachine.State.FALLING) and not curr_states.has(StateMachine.State.JUMPING)):
-		fall()
 	
 	if is_on_floor():
 		velocity.y = 0
@@ -103,34 +119,78 @@ func _physics_process(delta: float) -> void:
 		
 	if (curr_states.has(StateMachine.State.IDLE)):
 		move_speed = decel_curve.sample((curr_frame - decel_frame) / decel_duration) * max_speed
-		
+	
+	if (curr_states.has(StateMachine.State.DASHING)):
+		$Helpers/DashInvinciblity.start()
+		$Hurtbox/CollisionShape2D.set_deferred("disabled", true)
+		move_dash = prev_direction * dash_speed
+	
+	if (curr_frame - dash_frame) >= dash_duration:
+		curr_states.erase(StateMachine.State.DASHING)
+		move_dash = 0
+	
 	if (((curr_frame - jump_frame) >= jump_duration or is_on_ceiling() or Input.is_action_just_released("move_jump")) and curr_states.has(StateMachine.State.JUMPING)):
 		fall()
-		
-	if (!curr_states.has(StateMachine.State.JUMPING)):
+	
+	if (not is_on_floor() and !curr_states.has(StateMachine.State.JUMPING)):
 		fall()
 
-	if Input.is_action_just_pressed("move_jump") and is_on_floor():
-		jump()
+	if (Input.is_action_just_pressed("move_jump") or jump_buffered):
+		if $Helpers/JumpBufferer.is_stopped():
+			$Helpers/JumpBufferer.start()
+			jump_buffered = true
+		
+		if is_on_floor() or on_coyote:
+			jump()
 		
 	var move_direction: float = Input.get_axis("move_left", "move_right")
 	
-	velocity.x = move_direction * move_speed
+	if abs(move_direction) > 0:
+		prev_direction = move_direction
+	
+	velocity.x = move_direction * move_speed + move_dash
 	
 	if abs(move_direction) > 0 and (!curr_states.has(StateMachine.State.MOVING)) and (curr_states.has(StateMachine.State.IDLE)):
 		accel()
 		
-		
 	if move_direction == 0 and (curr_states.has(StateMachine.State.MOVING)) and (!curr_states.has(StateMachine.State.IDLE)):
 		decel()
+	
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dash()
 
 	was_on_floor = is_on_floor()
 	
 	move_and_slide()
-	change_state(curr_states.back())
+
+	if was_on_floor != is_on_floor() and $Helpers/CoyoteTimer.is_stopped():
+		$Helpers/CoyoteTimer.start()
+		on_coyote = true
+	
+	var temp = curr_states.back()
+	for state in curr_states:
+		if state > temp and is_on_floor():
+			temp = state
+	change_state(temp)
 
 func change_state(new_state: StateMachine.State):
 	if state_manager != null:
 		state_manager.queue_free()
 	state_manager = state_machine.get_state(new_state).new(self, change_state, $AnimatedSprite2D)
 	add_child(state_manager)
+
+func _on_jump_bufferer_timeout() -> void:
+	jump_buffered = false
+
+func _on_coyote_timer_timeout() -> void:
+	on_coyote = false
+
+func _on_dash_invinciblity_timeout() -> void:
+	$Hurtbox/CollisionShape2D.set_deferred("disabled", false)
+	$Helpers/DashCooldown.start()
+
+func _on_dash_cooldown_timeout() -> void:
+	can_dash = true
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	print(area)
--- states/state_machine.gd
+++ states/state_machine.gd
@@ -1,7 +1,8 @@
 class_name StateMachine
 
+# State[n] will overtake animation precedence over State[n-1]
 enum State {
-	IDLE, MOVING, JUMPING, FALLING
+	IDLE, FALLING, MOVING, JUMPING, DASHING
 }
 
 var state_equivalence
@@ -9,9 +10,10 @@ var state_equivalence
 func _init() -> void:
 	state_equivalence = {
 		State.IDLE : IdleState,
+		State.FALLING: IdleState,
 		State.MOVING: WalkState,
 		State.JUMPING: IdleState,
-		State.FALLING: IdleState,
+		State.DASHING: IdleState,
 	}
 
 func get_state(state: State):
@@ -19,4 +21,3 @@ func get_state(state: State):
 		return state_equivalence.get(state)
 	else:
 		printerr("No state ", str(state), " in state machine")
-		#return null
