976ded2ea4efd4a7490c0828cd2b41ece333df06
idotick <sanderdalangin@gmail.com>
2026-09-24 08:40:22 +0800

    fixed endless jump

--- states/player_states/stagger_state.gd
+++ states/player_states/stagger_state.gd
@@ -2,7 +2,7 @@ extends State
 
 class_name PlayerStagger
 
-@export var jump_delay: float = 0.2
+@export var jump_delay: float = 0.1
 
 @onready var player: CharacterBody2D = owner
 @onready var state_machine: StateMachine = get_parent()
@@ -11,10 +11,10 @@ class_name PlayerStagger
 @onready var is_jump : bool = false
 
 func enter() -> void:
+	is_jump = Input.is_action_just_pressed("move_jump")
 	sprite.play("stagger")
-	if Input.is_action_just_pressed("move_jump"):
+	if is_jump:
 		sprite.play("stagger", -(1/jump_delay), true)
-		is_jump = true
 	
 	if !(sprite.animation_finished.is_connected(switch_state)):
 		print("connected")
@@ -31,7 +31,6 @@ func update(_delta: float) -> void:
 	sprite.flip_h = player.prev_direction < 0
 
 func switch_state() -> void:
-	print(is_jump)
 	if is_jump:
 		state_machine.transition_to("Jump")
 	else:
